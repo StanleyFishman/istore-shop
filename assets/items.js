74b5e95037d71a7887e86dba7d7df560eed8abe2
StanleyFishman <117282489+StanleyFishman@users.noreply.github.com>
2025-09-12 09:25:28 +0500

    Link catalog tiles to individual item pages

--- assets/items.js
+++ assets/items.js
@@ -2,6 +2,26 @@ const ITEMS = {
   item1: {
     title: 'МАШИНА ЧЕШУИРОВАНИЯ',
     image: 'assets/images/item1_pic1.png'
+  },
+  item2: {
+    title: 'Барабанное оборудование',
+    image: 'assets/images/item2.png'
+  },
+  item3: {
+    title: 'Компрессорное оборудование',
+    image: 'assets/images/item3.png'
+  },
+  item4: {
+    title: 'Емкости и резервуары',
+    image: 'assets/images/item4.png'
+  },
+  item5: {
+    title: 'Теплообменное оборудование',
+    image: 'assets/images/airpods.png'
+  },
+  item6: {
+    title: 'iMac 24″',
+    image: 'assets/images/mac.png'
   }
 };
 
